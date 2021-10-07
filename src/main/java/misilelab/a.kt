b02d5940cd2ccc524e0dab2aebd4a1cfc60fd9d1
MisileLab <misileminecord@gmail.com>
2021-10-07 20:31:06 +0900

    delete a

--- src/main/java/misilelab/a.kt
+++
@@ -1,8 +0,0 @@
-internal class a {
-    val lastDamageCause: Unit
-
-    init {
-        val nEvent: EntityDamageByEntityEvent = event
-            .getEntity().getLastDamageCause() as EntityDamageByEntityEvent
-    }
-}
